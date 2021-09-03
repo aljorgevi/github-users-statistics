3ffac159f9d2cb2ac66cc94d64981192f132d2ab
Jorge Andrés Mena <jormencar@yahoo.com>
2021-09-03 14:07:08 +0100

    form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dashboard, Error } from './pages';
+import { Dashboard, Error, Login } from './pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path='/' component={Dashboard} />
+        <Route path='/login' component={Login} />
         <Route path='*' component={Error} />
       </Switch>
     </Router>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Dashboard from './Dashboard';
 import Error from './Error';
+import Login from './Login';
 
-export { Dashboard, Error };
+export { Dashboard, Error, Login };
